c4e0f2c51f86ae9e594a9890815b69886f1a27ed
Oleksandr Untervald <72872331+AlexanderUntervald@users.noreply.github.com>
2023-09-09 20:46:32 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 //next image
 import Image from "next/image";
 
+
 // components
 import ParticlesContainer from '../components/ParticlesContainer';
 import ProjectsBtn from '../components/ProjectsBtn';
@@ -18,6 +19,7 @@ const Home = () => {
     <div className='bg-primary/60 py-32 h-full'> 
        {/* text */}
        <div className='w-full h-full bg-gradient-to-r from-primary/10 via-black/30 to-black/10'> 
+       
           <div className='text-center flex-col justify-center xl:pt-40 xl:text-left h-full container mx-auto'>
              {/* title */}
              <motion.h1 
@@ -40,26 +42,17 @@ const Home = () => {
               </motion.p>
               {/* btn */}
               <div className='flex justify-center xl:hidden relative'>
-               <ProjectsBtn />
+        
               </div>
-              <motion.div 
-                  variants={fadeIn('down', 0.4)} 
-                  initial='hidden' 
-                  animate='show' 
-                  exit='hidden'
-                  className="hidden xl:flex"
-                  >
-                 <ProjectsBtn/>
-              </motion.div>
-          </div>
-       </div>
-       {/* image */}
-       <div className='w-[1200px] w-full h-full absolute right-0 bottom-0'>
+            
+             
+      <div className='w-[1200px] w-full h-full absolute right-0 bottom-0'>
          <div className='bg-none xl:bg-explosion xl:bg-cover xl:bg-right xl:bg-no-repeat w-full h-full absolute mix-blend-color-dodge translate-z-0'>
              
          </div>
+         
         <ParticlesContainer />
-
+        
          <motion.div 
               variants={fadeIn('up', 0.5)} 
               initial='hidden' 
@@ -68,10 +61,32 @@ const Home = () => {
               transition={{duration: 1, ease: 'easeInOut'}}
               className='w-full h-full max-w-[737px] max-h-[678px] absolute -bottom-32 lg:bottom-0 lg:right-[8%]'>
             <Avatar />
-           
+            <div className='flex justify-center items-center w-full h-full'>
+                 {/* Розмістіть ProjectsBtn у центрі */}
+                 <ProjectsBtn />
+               </div>
          </motion.div>
-         </div>
+        
+         </div> 
+          
+    
+         <motion.div 
+                  variants={fadeIn('up', 0.4)} 
+                  initial='hidden' 
+                  animate='show' 
+                  exit='hidden'
+                  className="hidden xl:flex"
+                  >
+                     <ProjectsBtn  /> 
+              </motion.div>
+       
+          </div>
+       </div>
+       {/* image */}
+
+        
     </div>
+   
   );
 };
 
