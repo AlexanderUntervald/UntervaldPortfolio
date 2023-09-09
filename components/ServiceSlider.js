8117f2dcba8d19e4e6498a180f6219bb2e400a11
Oleksandr Untervald <72872331+AlexanderUntervald@users.noreply.github.com>
2023-09-09 20:53:34 +0300

    Update ServiceSlider.js

--- components/ServiceSlider.js
+++ components/ServiceSlider.js
@@ -68,7 +68,7 @@ const ServiceSlider = () => {
       clickable: true,
     }}
     modules={[FreeMode, Pagination]}
-    className='h-[290px] sm:h-[390px]'
+    className='h-[390px] sm:h-[520px]'
     >
 
       {
