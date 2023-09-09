7bc17cf0f78fc6202c561ffab0a39df255d315f8
Oleksandr Untervald <72872331+AlexanderUntervald@users.noreply.github.com>
2023-09-09 20:48:56 +0300

    Update Avatar.js

--- components/Avatar.js
+++ components/Avatar.js
@@ -1,14 +1,18 @@
 import Image from "next/image";
 
 const Avatar = () => {
+  const customStyle = {
+    transform: 'translate(195px, 43px)',
+  };
+
   return (
-    <div className='hidden xl:flex xl:max-w-none'>
+    <div className='hidden xl:flex xl:max-w-none' style={customStyle}>
       <Image 
-      src={'/avatar3.png'}
-      width={737}
-      height={678}
-      alt=""
-      className="translate-z-0 w-full h-full"
+        src={'/avatar4.png'}
+        width={737}
+        height={678}
+        alt=""
+        className="translate-z-0 w-full h-full "
       /> 
     </div>
   )
